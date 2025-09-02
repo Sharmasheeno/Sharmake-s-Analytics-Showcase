c5c4fd350c74624cfe10aa673128073cff5d8f18
Shaka Hassan Said <shakahassansaid@gmail.com>
2025-09-02 08:35:26 +0000

    My Presence in the Data Community   use here real png icons like courser

--- src/components/icons/coursera-icon.tsx
+++ src/components/icons/coursera-icon.tsx
@@ -3,12 +3,14 @@ import type { SVGProps } from 'react';
 export function CourseraIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        viewBox="0 0 24 24"
-        {...props}>
-        <path fill="currentColor" d="M12.012 2.25c-5.18 0-9.375 4.095-9.375 9.15c0 4.425 3.12 8.1 7.23 8.925v-6.095H7.752V12h2.115V9.99c0-2.093 1.283-3.225 3.165-3.225c.9 0 1.672.067 1.898.1v2.16l-1.275.001c-1.012 0-1.214.48-1.214 1.185V12h2.392l-.314 2.25h-2.078v6.284c4.32-.7 7.5-4.5 7.5-8.929c0-5.055-4.192-9.15-9.375-9.15"></path>
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path fill="#0056D2" d="M12,2C6.48,2,2,6.48,2,12s4.48,10,10,10s10-4.48,10-10S17.52,2,12,2z M17.2,15.2c-0.3-1-1.1-1.6-2.1-1.6c-1,0-1.8,0.6-2.1,1.6h-2.2V8.8h2.2v2.9c0.6-0.9,1.6-1.4,2.6-1.4c1.8,0,3.2,1.5,3.2,3.4S19,15.2,17.2,15.2z"></path>
+      <path fill="#0056D2" d="M8.8,15.2H6.5V8.8h2.2v6.4H8.8z"></path>
     </svg>
   );
 }
